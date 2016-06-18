15771b004be614870f320bfb54bb1b95d388dba4
vsoch <vsochat@stanford.edu>
2016-06-18 16:29:28 -0700

    finishing date slider, next want to return important keywords

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Resource, Api
 from random import choice
 import itertools
 import random
@@ -15,6 +16,10 @@ class BodyMapServer(Flask):
     def __init__(self, *args, **kwargs):
             super(BodyMapServer, self).__init__(*args, **kwargs)
 
+            # Date range to start with (loaded data)
+            self.start_date = "3/20/2010"
+            self.end_date = "5/20/2012"
+
             # load data on start of application
             self.bodymap = "".join(open("data/bodymap.svg","r").readlines())
             self.labels = json.load(open("data/simpleFMA.json","r"))
@@ -24,14 +29,47 @@ class BodyMapServer(Flask):
                                                     "LATITUDE_EPSG3857","LONGITUDE_EPSG3857",
                                                     "LOCATION_IMPORTANCE","ALTITUDE"],axis=1)
             self.fatalities = self.fatalities.rename(index=str, columns={"Unnamed: 0":"id"})
+
+            # This takes a bit of computation and should be run at startup
+            self.fatalities['INCIDENT_DATETIME'] = pandas.to_datetime(self.fatalities["INCIDENT_DATE"])
+
             # generate list of descriptions associated with ids
             self.descriptions = self.fatalities.DESCRIPTION.copy()    
             self.descriptions.index = self.fatalities.id.tolist()  
             self.descriptions = self.descriptions.to_dict()      
 
 app = BodyMapServer(__name__)
+api = Api(app)
 
-# Global variables and functions
+# API VIEWS ##########################################################################################
+
+class apiIndex(Resource):
+    """apiIndex
+    Main view for REST API to display all available fatalities data
+    """
+    def get(self):
+        fatalities_json = app.fatalities.to_dict(orient="records")
+        fatalities_json = parse_unicode(fatalities_json)
+        return fatalities_json
+
+class apiQueryDates(Resource):
+    """apiQueryDates
+    return a list of ids for points that are within a range of dates
+    /api/dates/0/6/2010/0/6/2010
+    """
+    def get(self, sm, sd, sy, em, ed, ey):
+        start_date = pandas.to_datetime("%s/%s/%s" %(sm,sd,sy))
+        end_date = pandas.to_datetime("%s/%s/%s" %(em,ed,ey))
+        subset = app.fatalities[app.fatalities.INCIDENT_DATETIME >= start_date]
+        subset = subset[subset.INCIDENT_DATETIME <= end_date]
+        return {"ids": subset.id.tolist()}
+              
+# Add all resources
+api.add_resource(apiIndex,'/api/deaths') # start month, day, year / end month, date, year
+api.add_resource(apiQueryDates,'/api/dates/<int:sm>/<int:sd>/<int:sy>/<int:em>/<int:ed>/<int:ey>')
+
+
+# Global variables and functions #####################################################################
 
 def parse_unicode(meta):
     '''parse_unicode: decodes to utf-8 and encodes in ascii, replacing weird characters with empty space.
@@ -44,15 +82,10 @@ def parse_unicode(meta):
     for entry in meta:
         new_entry = dict()
         for key,value in entry.iteritems():
-            if key == "authors":
-                authors = value.decode('utf-8').encode('ascii',errors='replace').replace('?',' ')
-                new_entry[key] = ", ".join(parse_authors(authors))
-            elif key == "score": # This is a float
+            if isinstance(value,int) or isinstance(value,float):
                 new_entry[key] = value
-            elif key == "url": # Don't want to replace ? with space
-                new_entry[key] = value.decode('utf-8').encode('ascii',errors='replace')                 
             else:
-                new_entry[key] = value.decode('utf-8').encode('ascii',errors='replace').replace('?',' ')
+                new_entry[key] = unicode(value,"ISO-8859-1").encode('ascii',errors='replace').replace('?','')
         parsed.append(new_entry)
     return parsed
 
@@ -66,6 +99,8 @@ def random_colors(concepts):
         colors[concept] = '#%02X%02X%02X' % (r(),r(),r())
     return colors
 
+
+# Views ##############################################################################################
 
 @app.route("/")
 def index():
@@ -99,17 +134,10 @@ def bodymap():
     for part,idx in app.deaths.iteritems():
         deaths[str(part)] = idx
 
-    # Get a list of dates
-    dates = app.fatalities.INCIDENT_DATE.tolist()
-    dates.sort()
-
     return render_template("map.html",bodymap=app.bodymap,
                                       labels=app.labels,
                                       deaths=deaths,
-                                      descriptions=app.descriptions,
-                                      start_date=dates[0],
-                                      end_date=dates[-1])
-
+                                      descriptions=app.descriptions)
 if __name__ == "__main__":
     app.debug = True
     app.run()
