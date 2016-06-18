d643ee7618148516c24021c1c816ecde8c171f69
vsoch <vsochat@stanford.edu>
2016-06-18 14:11:45 -0700

    adding start of timeline, next going to add api to serve updated data

--- index.py
+++ index.py
@@ -73,16 +73,42 @@ def index():
     return render_template("index.html",bodymap=app.bodymap,
                                         labels=app.labels)
 
+@app.route("/detail")
+def detail():
+    '''view details for a set of ids'''
+    # We will render dates across the bottom along with general counts
+    # need lookup with date --> ids
+    dates = app.fatalities["INCIDENT_DATE"].copy()
+    dates.index = app.fatalities["id"]
+    dates = pandas.DataFrame(dates).groupby(by="INCIDENT_DATE").groups
+
+    df = pandas.DataFrame(columns=["date","count"])
+    df["date"] = dates.keys()
+    df["count"] = [len(dates[x]) for x in dates.keys()]
+
+    return render_template("brush.html",bodymap=app.bodymap,
+                                        labels=app.labels,
+                                        descriptions=app.descriptions,
+                                        dates=df.to_dict(orient="records"),
+                                        lookup=dates)
+
 @app.route("/map")
 def bodymap():
     '''view deaths via bodymap'''
     deaths = dict()
     for part,idx in app.deaths.iteritems():
         deaths[str(part)] = idx
+
+    # Get a list of dates
+    dates = app.fatalities.INCIDENT_DATE.tolist()
+    dates.sort()
+
     return render_template("map.html",bodymap=app.bodymap,
                                       labels=app.labels,
                                       deaths=deaths,
-                                      descriptions=app.descriptions)
+                                      descriptions=app.descriptions,
+                                      start_date=dates[0],
+                                      end_date=dates[-1])
 
 if __name__ == "__main__":
     app.debug = True
